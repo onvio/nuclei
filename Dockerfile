52b9774af6733cc3e792b836a34c7df2226e87f8
Bryan <b.soman@onvio.nl>
2021-03-15 18:25:45 +0100

    Fix for parsing new version nuclei

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
 FROM projectdiscovery/nuclei:latest
 USER root
-COPY start.sh nuclei_seqhub.py /app/
-WORKDIR /app
+COPY start.sh nuclei_seqhub.py /opt/nuclei/
+WORKDIR /opt/nuclei/
 RUN apk update \
     && apk --no-cache add git bash python3 ca-certificates jq \
-    && chmod +x start.sh \
-    && git clone https://github.com/projectdiscovery/nuclei-templates.git
-COPY .nuclei-ignore nuclei-templates/
-VOLUME /var/reports/
-ENTRYPOINT ["./start.sh"]
+    && git clone https://github.com/projectdiscovery/nuclei-templates.git \
+    && chmod +x /opt/nuclei/start.sh
+COPY .nuclei-ignore /opt/nuclei/nuclei-templates/
+ENTRYPOINT ["/opt/nuclei/start.sh"]
--- nuclei_seqhub.py
+++ nuclei_seqhub.py
@@ -1,7 +1,8 @@
+from io import SEEK_CUR
 import json
+import sys
 
-
-with open('/var/reports/nuclei.json', 'r') as jsondict:
+with open('/var/reports/nuclei_formatted.json', 'r') as jsondict:
     findings = json.load(jsondict)
 
 seqhubreportpath = "/var/reports/seqhubreport.json"
@@ -9,20 +10,22 @@ seqhubreportpath = "/var/reports/seqhubreport.json"
 
 def seqhubreport():
     jsonobject = {"vulnerabilities": []}
-    for i in findings["vulnerabilities"]:
-        severity = i.get('severity')
-        if i.get('severity') == 'critical':
-            severity = 'high'
-        description = i.get('description')
+    for vuln in findings["vulnerabilities"]:
+        severity = vuln['info']['severity']
+        'low' if severity == 'info' else severity
+        'high' if severity == 'critical' else severity
+        print(severity)
+        description = vuln['info'].get('description')
         if not description:
-            description = i.get('template')
-        if i.get('extracted_results'):
-            extracted_results_str = ' '.join([str(e.replace('"', '')) for e in i.get('extracted_results')])
+            description = vuln.get('templateID')
+        if vuln.get('extracted_results'):
+            extracted_results_str = ' '.join([str(e.replace('"', '')) for e in vuln.get('extracted_results')])
             description = description + ', ' + extracted_results_str
+        print(description)
         with open(seqhubreportpath, 'w'):
             secretsdb = {
-                "title": i.get('name'),
-                "description": f"{i.get('matched')}, {description}",
+                "title": vuln.get('templateID'),
+                "description": f"{vuln.get('matched')}, {description}",
                 "severity": severity,
             }
             jsonobject['vulnerabilities'].append(secretsdb)
