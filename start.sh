d022fe1d53e7b39b01110f3165042f8a1c585978
Bryan <b.soman@onvio.nl>
2021-03-16 08:45:43 +0100

    silent mode exclude tech detects

--- start.sh
+++ start.sh
@@ -4,13 +4,13 @@ set -x
 # ./nuclei -update-templates
 mkdir /var/reports
 # Scan # -mc 200,301,401,403,405
-nuclei -target $1 \
+./nuclei -target $1 \
     -H "X-Scanner: Nuclei" \
     -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0" \
     -t nuclei-templates/ \
-    -t nuclei-templates/miscellaneous/ \
-    -exclude technologies/tech-detect.yaml \
     -json \
+    -exclude nuclei-templates/technologies/tech-detect.yaml \
+    -silent \
     -o /var/reports/nuclei.json \
 
 # Convert Nuclei
