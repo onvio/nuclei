9482b1e062f2ea5bccd85b9e4b22972eed85d670
bryan <b.soman@onvio.nl>
2021-03-16 08:37:52 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ set -x
 # ./nuclei -update-templates
 mkdir /var/reports
 # Scan # -mc 200,301,401,403,405
-nuclei -target $1 \
+./nuclei -target $1 \
     -H "X-Scanner: Nuclei" \
     -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0" \
     -t nuclei-templates/ \
