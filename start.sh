668e464e61203f234dc2f617e52b54e3dbf74c5c
bryan <b.soman@onvio.nl>
2021-06-22 11:13:12 +0200

    Update start.sh
    proper excludes

--- start.sh
+++ start.sh
@@ -7,7 +7,8 @@ nuclei -target $1 \
     -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0" \
     -t /root/nuclei-templates/ \
     -json \
-    -exclude nuclei-templates/technologies/tech-detect.yaml \
+    -exclude /root/nuclei-templates/technologies/tech-detect.yaml \
+    -exclude /root/nuclei-templates/technologies/waf-detect.yaml \
     -silent \
     -o /var/reports/nuclei.json
 
