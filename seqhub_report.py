fffa0e6efd5d8e7d1f1367ebcf9c638012fbe673
Bryan <b.soman@onvio.nl>
2021-06-30 09:28:52 +0200

    Better error handling for loading nuclei reports

--- seqhub_report.py
+++ seqhub_report.py
@@ -1,43 +1,54 @@
 import json
 import os.path
 
-def parse_report(nuclei_report, seqhub_report):
-    nuclei_findings  = []
-    seqhub_findings = { "vulnerabilities": [] }
 
-    if os.path.isfile(nuclei_report):
+def parse_report(nuclei_report, seqhub_report):
+
+    nuclei_findings = []
+    seqhub_findings = {"vulnerabilities": []}
+
+    if not os.path.isfile(nuclei_report):
+        return print("Nuclei report not found. Error during scan ?")
+    if os.path.getsize(nuclei_report) == 0:
+        return print("Nuclei report is empty. 0 Results ?")
+
+    try:
         with open(nuclei_report, 'r') as file:
             nuclei_json = file.read()
             # Nuclei json output is not valid json, so fix it
             nuclei_json = f"[{nuclei_json}]"
             nuclei_json = nuclei_json.replace("}\n", "},")
-        
+
             # remove the last comma to get a properly formatted json
             last_char_index = nuclei_json.rfind(",")
             nuclei_json = nuclei_json[:last_char_index] + nuclei_json[last_char_index+1:]
 
             nuclei_findings = json.loads(nuclei_json)
+        for vuln in nuclei_findings:
+            info = vuln['info']
+            severity = info['severity']
+            if severity == 'info':
+                severity = 'low'
+            if severity == 'critical':
+                severity = 'high'
 
-    for vuln in nuclei_findings:
-        info = vuln['info']
-        severity = info['severity']
-        if severity == 'info':
-            severity = 'low'
-        if severity == 'critical':
-            severity = 'high'
+            description = ''
+            if 'description' in info:
+                description = info['description']
 
-        description = ''
-        if 'description' in info:
-            description = info['description']
+            with open(seqhub_report, 'w'):
+                seqhub_findings["vulnerabilities"].append({
+                    "title": info['name'],
+                    "description": f"{vuln['matched']} {description} ({vuln['templateID']})",
+                    "severity": severity,
+                })
 
-        with open(seqhub_report, 'w'):
-            seqhub_findings["vulnerabilities"].append({
-                "title": info['name'],
-                "description": f"{vuln['matched']} {description} ({vuln['templateID']})",
-                "severity": severity,
-            })
+            with open(seqhub_report, 'w') as f:
+                json.dump(seqhub_findings, f, indent=4)
+    except Exception as ex:
+        error = "Error Parsing Nuclei JSON Report. An exception of type {0} occurred. Arguments:\n{1!r}"
+        error = error.format(type(ex).__name__, ex.args)
+        print(error)
 
-    with open(seqhub_report, 'w') as f:
-        json.dump(seqhub_findings, f, indent=4)
 
 parse_report('/var/reports/nuclei.json', '/var/reports/seqhub.json')
