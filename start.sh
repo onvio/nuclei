cf12e2752bace634c8761b6040c47b7a6ff6e7a6
bryan <b.soman@onvio.nl>
2022-01-05 11:59:34 +0100

    Update start.sh
    new exclude parameter

--- start.sh
+++ start.sh
@@ -7,9 +7,9 @@ nuclei -target $1 \
     -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0" \
     -t /root/nuclei-templates/ \
     -json \
-    -exclude /root/nuclei-templates/technologies/tech-detect.yaml \
-    -exclude /root/nuclei-templates/technologies/waf-detect.yaml \
-    -exclude /root/nuclei-templates/misconfiguration/http-missing-security-headers.yaml \
+    -et /root/nuclei-templates/technologies/tech-detect.yaml \
+    -et /root/nuclei-templates/technologies/waf-detect.yaml \
+    -et /root/nuclei-templates/misconfiguration/http-missing-security-headers.yaml \
     -o /var/reports/nuclei.json
 
 # Parse Report for SEQHUB
